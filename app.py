0de3e2d9e96ac61abc20b93a86247515356fcb56
Nguyen, Le <caonguyenle007@gmail.com>
2024-02-24 13:00:00 +0700

    Fix make lint test

--- app.py
+++ app.py
@@ -23,18 +23,13 @@ def home():
     html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
-# TO DO:  Log out the prediction value
 @app.route("/predict", methods=['POST'])
 def predict():
     # Performs an sklearn prediction
-    try:
-        # Load pretrained model as clf. Try any one model. 
-        # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
-        # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
-        LOG.info("JSON payload: %s json_payload")
-        return "Model not loaded"
+    # Load pretrained model as clf. Try any one model. 
+    # clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
+    # clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
+    clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
 
     json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
